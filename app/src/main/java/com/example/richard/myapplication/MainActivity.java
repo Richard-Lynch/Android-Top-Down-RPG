22660d53a5a6264ec5b04f51bbf1e4dae3dfbf9a
Ste <stephenkelehan@gmail.com>
2017-01-20 15:57:58 +0000

    Ste's commit

--- app/src/main/java/com/example/richard/myapplication/MainActivity.java
+++ app/src/main/java/com/example/richard/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //just seeing if this commit is working
+        // Ste seen to
     }
 }
