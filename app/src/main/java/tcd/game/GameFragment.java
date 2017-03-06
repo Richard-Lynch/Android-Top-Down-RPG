2c21f3fff3f84fc618d5e17fb82250db15d25ffc
Richard-Lynch <rich753@hotmail.com>
2017-03-06 20:35:48 +0000

    Fixed the Init Issue

--- app/src/main/java/tcd/game/GameFragment.java
+++ app/src/main/java/tcd/game/GameFragment.java
@@ -97,12 +97,16 @@ public class GameFragment extends Fragment{
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "Initializing: in onActCreate");
+
         getView().getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
+                Log.d(TAG, "Initializing: in onGlobalLayout");
                 int width = getView().getWidth();
                 int height = getView().getHeight();
                 initializeGameMode(width, height);
+                getView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
         });
     }
@@ -141,7 +145,8 @@ public class GameFragment extends Fragment{
     }
 
 
-    /***********************************************************************************
+    /*
+     **********************************************************************************
      * Main Game Methods
      ***********************************************************************************/
 
@@ -253,7 +258,7 @@ public class GameFragment extends Fragment{
         public void run() {
             Log.d(TAG, "Run Method starting");
             while (!gameInitialized) {
-
+                Log.d(TAG, "Waiting for game to Initialise");
             }
             while (running) {
 //                    Log.d(TAG,"GameLoop Starting");
