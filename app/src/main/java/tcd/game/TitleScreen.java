82ab7d9c94c089653d80893dc76953bf12871f97
Davide Cavion <davide@cavion.eu>
2017-03-11 23:03:24 +0000

    Small improvements to loading screen, added background and loading bar

--- app/src/main/java/tcd/game/TitleScreen.java
+++ app/src/main/java/tcd/game/TitleScreen.java
@@ -5,19 +5,64 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.os.Handler;
 
 public class TitleScreen extends AppCompatActivity {
+
+    public ProgressBar progressBar;
+    int progressStatus;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
+
+        progressBar = (ProgressBar) findViewById(R.id.splash_progress);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 
+    public void startGameClk(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setProgress(0);
 
-    public void startGameClk(View view){
-        Intent i = new Intent(this,Game.class);
-        startActivity(i);
+        new Thread(new Runnable() {
+            public void run() {
+
+                // Start a new thread starting the game
+
+                handler.post(new Runnable() {
+                    public void run() {
+                        Intent intent = new Intent(getBaseContext(), Game.class);
+                        startActivity(intent);
+                    }
+                });
+
+                // This way we can execute the cycle below while the game is loading
+
+                while (progressStatus + 10 < 100) {
+                    progressStatus += 5;
+
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+
+                            // Progress is updated every 50ms, however this
+                            // is not reflected on the screen since that the
+                            // view is not updated. Couldn't find a way to force
+                            // the update, postInvalidate/invalidate are not working.
+                            // I'm missing something.
+                        }
+                    });
+
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
-
 }
